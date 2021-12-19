6cc59216b853cd51c07d1a3218c79ae10235d4d4
Neivis Franco Reyes <franeyrey1814@gmail.com>
2021-12-19 12:06:14 -0500

    cambios

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  user = 'assets/imagenes/user.png';
   constructor() { }
 
   ngOnInit(): void {
