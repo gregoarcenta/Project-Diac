99c3a267cb95fad704f0e76998deacde78d38981
Neivis Franco Reyes <franeyrey1814@gmail.com>
2021-12-20 00:39:02 -0500

    arreglos de las interfaces

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -4,7 +4,8 @@ import { DestrezaComponent } from '../destreza/page/destreza/destreza.component'
 import { EstudianteComponent } from '../estudiante/page/estudiante/estudiante.component';
 import { EstudianteListComponent } from '../estudiante-list/page/estudiante-list/estudiante-list.component';
 import { DefaultDashboardComponent } from './default-dashboard/default-dashboard.component';
-
+import { InstitucionComponent } from '../institucion/page/institucion/institucion.component';
+import { FechaElaboracionComponent } from '../fecha-elaboracion/page/fecha-elaboracion/fecha-elaboracion.component';
 const routes: Routes = [
   {
     path: '',
@@ -14,13 +15,25 @@ const routes: Routes = [
         path: 'destreza',
         component: DestrezaComponent
       },
+      
+      {
+        path: 'estudiante-list',
+        component: EstudianteListComponent
+      },
+
       {
         path: 'estudiante',
         component: EstudianteComponent
       },
+      
       {
-        path: 'estudiante-list',
-        component: EstudianteListComponent
+        path: 'estudiante/institucion',
+        component: InstitucionComponent
+      },
+      
+      {
+        path: 'estudiante/institucion/fecha-elaboracion',
+        component: FechaElaboracionComponent
       }
     ],
   },
