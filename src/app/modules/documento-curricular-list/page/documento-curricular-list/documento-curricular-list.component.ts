d8abd188b724ae3d1f87b0e69da582e748f9dbf0
Neivis Franco Reyes <franeyrey1814@gmail.com>
2022-02-09 14:07:25 -0500

    cambios en el pdf

--- src/app/modules/documento-curricular-list/page/documento-curricular-list/documento-curricular-list.component.ts
+++ src/app/modules/documento-curricular-list/page/documento-curricular-list/documento-curricular-list.component.ts
@@ -68,31 +68,35 @@ export class DocumentoCurricularListComponent implements OnInit {
             widths: [245, 245],
             body: [
               [{ text: 'DOCUMENTO INDIVIDUAL DE ADAPTACIÓN CURRICULAR', style: 'tableHeader', colSpan: 2, alignment: 'center' }, {}],
+              
               [{ text: '1. DATOS DE IDENTIFICACIÓN DEL ESTUDIANTE', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: `Padre / Tutor Legal: ${doc.Student.tutor}`, style: 'tableHeader', colSpan: 2, }, {}],
               [{ text: `Nombres: ${doc.Student.nameStudent}`, style: 'tableHeader', colSpan: 1 }, { text: `Apellidos: ${doc.Student.lastNameStudent}`, colSpan: 1 }],
-              [{ text: 'Edad: 17 AÑOS 7 MESES ', style: 'tableHeader', colSpan: 1 }, { text: 'Fecha y Lugar de Nacimiento: 11/11/03 MANTA', colSpan: 1 }],
-              [{ text: 'Número de hermanos: 2', style: 'tableHeader', colSpan: 1 }, { text: 'Lugar que ocupa: 2', colSpan: 1 }],
-              [{ text: 'Nombre de la madre: PATRICIA JÁCOME', style: 'tableHeader', colSpan: 1 }, { text: ' Nombre del padre: DAVID GARCES', colSpan: 1 }],
-              [{ text: 'Domicilio:', style: 'tableHeader', colSpan: 1 }, { text: 'CALLE 8 AVENIDA 14', colSpan: 1 }],
-              [{ text: 'Ciudad: ', style: 'tableHeader', colSpan: 1 }, { text: 'MANTA', colSpan: 1 }],
-              [{ text: 'Provincia:', style: 'tableHeader', colSpan: 1 }, { text: 'MANABÍ', colSpan: 1 }],
-              [{ text: 'Código Postal:', style: 'tableHeader', colSpan: 1 }, { text: '130802', colSpan: 1 }],
-              [{ text: 'Teléfono:', style: 'tableHeader', colSpan: 1 }, { text: '0989688108', colSpan: 1 }],
-              [{ text: 'Grado o curso:  DÉCIMO              Paralelo: “A”', style: 'tableHeader', colSpan: 1 }, { text: 'Año lectivo: 2021-2022', colSpan: 1 }],
+              [{ text: `Edad:${doc.Student.edad}`, style: 'tableHeader', colSpan: 1 }, { text: `Fecha y Lugar de Nacimiento:${doc.Student.dateOfBirth}`, colSpan: 1 }],
+              [{ text: `Número de hermanos:${doc.Student.numBrothers}`, style: 'tableHeader', colSpan: 1 }, { text: `Lugar que ocupa:${doc.Student.placeOccupies}`, colSpan: 1 }],
+              [{ text: `Nombre de la madre: ${doc.Student.nameMother}`, style: 'tableHeader', colSpan: 1 }, { text: `Nombre del padre:${doc.Student.nameFather}`, colSpan: 1 }],
+              [{ text: 'Domicilio:', style: 'tableHeader', colSpan: 1 }, { text: `${doc.Student.placeOccupies}`, colSpan: 1 }],
+              [{ text: 'Ciudad: ', style: 'tableHeader', colSpan: 1 }, { text: `${doc.Student.town}` , colSpan: 1 }],
+              [{ text: 'Provincia:', style: 'tableHeader', colSpan: 1 }, { text: `${doc.Student.province}` , colSpan: 1 }],
+              [{ text: 'Código Postal:', style: 'tableHeader', colSpan: 1 }, { text: `${doc.Student.postalCode}` , colSpan: 1 }],
+              [{ text: 'Teléfono:', style: 'tableHeader', colSpan: 1 }, { text:`${doc.Student.phone}` , colSpan: 1 }],
+              [{ text: `Grado o curso:${doc.Student.course}` , style: 'tableHeader', colSpan: 1 }, { text:`Paralelo:${doc.Student.parallel}` , colSpan: 1 }],
+              [{ text: `Año lectivo: ${doc.Student.schoolYear}` , colSpan: 1 }, {}],
 
+              //------Registro de la institucion----------
               [{ text: '2. DATOS DE IDENTIFICACIÓN DE LA INSTITUCIÓN EDUCATIVA', style: 'tableHeader', colSpan: 2 }, {}],
-              [{ text: 'Nombre: UE REPÚBLICA DEL ECUADOR', style: 'tableHeader', colSpan: 1 }, { text: 'Distrito No.: 13D02', colSpan: 1 }],
-              [{ text: 'Dirección:', style: 'tableHeader', colSpan: 1 }, { text: 'CALLE 17 Y AVENIDA 35', colSpan: 1 }],
-              [{ text: 'Localidad: MANTA', style: 'tableHeader', colSpan: 1 }, { text: 'Código Postal: 130802', colSpan: 1 }],
-              [{ text: 'Teléfono:', style: 'tableHeader', colSpan: 1 }, { text: '', colSpan: 1 }],
-              [{ text: 'Modalidad: MATUTINA', style: 'tableHeader', colSpan: 1 }, { text: ' Tipo:       FISCAL', colSpan: 1 }],
+              [{ text: `Nombre: ${doc.Institution.nameInstitution}`, style: 'tableHeader', colSpan: 1 }, { text: `Distrito No.: ${doc.Institution.district}`, colSpan: 1 }],
+              [{ text: 'Dirección:', style: 'tableHeader', colSpan: 1 }, { text: ` ${doc.Institution.address}`, colSpan: 1 }],
+              [{ text: `Localidad: ${doc.Institution.locationInstitution}`, style: 'tableHeader', colSpan: 1 }, { text: `Código Postal:${doc.Institution.postalCode}`, colSpan: 1 }],
+              [{ text: 'Teléfono:', style: 'tableHeader', colSpan: 1 }, { text: `${doc.Institution.phone}`, colSpan: 1 }],
+              [{ text: `Modalidad: ${doc.Institution.modality}`, style: 'tableHeader', colSpan: 1 }, { text: ` Tipo:${doc.Institution.type}`, colSpan: 1 }],
 
+              //------Fecha de elvaboracion----------
               [{ text: '3. FECHA DE ELABORACIÓN Y DURACIÓN PREVISTA', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: 'Fecha de elaboración: ', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: 'Fecha de actualización: 21/07/2021', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: 'Duración prevista: Año lectivo 2021-2022', style: 'tableHeader', colSpan: 2 }, {}],
-
+              //------Areas / materias----------
               [{ text: '4. ÁREAS / MATERIAS OBJETO DE ADAPTACIÓN CURRICULAR', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: 'Área / Materia', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: 'Lengua y Literatura', colSpan: 2 }, {}],
@@ -106,11 +110,12 @@ export class DocumentoCurricularListComponent implements OnInit {
               [{ text: '5. PROFESIONALES IMPLICADOS EN LA REALIZACIÓN DEL DOCUMENTO DE ADAPTACIÓN CURRICULAR', style: 'tableHeader', colSpan: 2 }, {}],
               [{ text: 'Nombre:', style: 'tableHeader', colSpan: 1 }, { text: 'Función:', style: 'tableHeader', colSpan: 1 }],
               [{ text: 'Lic. Celia Williams Carranza', colSpan: 1 }, { text: 'Docente de Lengua y Literatura', colSpan: 1 }],
-              // [{text: 'Lic. Virginia Menéndez Rivadeneira', colSpan: 1}, {text: 'Docente de Inglés',colSpan: 1}],
-              // [{text: 'Lic. Karen Toro Mendoza', colSpan: 1}, {text: 'Docente de Ciencias Naturales',colSpan: 1}],
-              // [{text: 'Lic. Narcisa Bastidas Mora', colSpan: 1}, {text: 'Docente de Educación Física',colSpan: 1}],
-              // [{text: 'Lic. Cinthia Burau Carreño', colSpan: 1}, {text: 'Docente de Matemáticas y Proyecto Educativo',colSpan: 1}],
-              // [{text: 'Lic. Jairo Vélez Cedeño', colSpan: 1}, {text: 'Docente de Estudios Sociales',colSpan: 1}],
+              [{text: 'Lic. Virginia Menéndez Rivadeneira', colSpan: 1}, {text: 'Docente de Inglés',colSpan: 1}],
+              [{text: 'Lic. Karen Toro Mendoza', colSpan: 1}, {text: 'Docente de Ciencias Naturales',colSpan: 1}],
+              [{text: 'Lic. Narcisa Bastidas Mora', colSpan: 1}, {text: 'Docente de Educación Física',colSpan: 1}],
+              [{text: 'Lic. Cinthia Burau Carreño', colSpan: 1}, {text: 'Docente de Matemáticas y Proyecto Educativo',colSpan: 1}],
+              [{text: 'Lic. Jairo Vélez Cedeño', colSpan: 1}, {text: 'Docente de Estudios Sociales',colSpan: 1}],
+              
 
 
             ]
