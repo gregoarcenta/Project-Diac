9557edfc821b029071dfc0c6d37da2627c2be8fe
Neivis Franco Reyes <franeyrey1814@gmail.com>
2022-02-08 21:27:06 -0500

    cambios para la tabla de documentacion

--- src/app/modules/documento-curricular-list/page/documento-curricular-list/documento-curricular-list.component.ts
+++ src/app/modules/documento-curricular-list/page/documento-curricular-list/documento-curricular-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentoCurricularListService } from '../../service/documento-curricular-list.service'; 
+// importacion de pdf 
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { Doc, Student } from '../../interfaces/documento.interface';
 
 @Component({
   selector: 'app-estudiante-list',
@@ -6,27 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documento-curricular-list.component.css']
 })
 export class DocumentoCurricularListComponent implements OnInit {
-
+  
+  documentoList: Doc[] = [];
+  estudianteList: Student[] = [];
 //------paginacion----
 pageActual: number = 1;
 
   // -------filtro de pipe
   filterItems = '';
 
-items: any = [
-  { id:"1", nombre:"Alex ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"2", nombre:"Neivis ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"3", nombre:"Bryan ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"4", nombre:"Adrian ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"5", nombre:"neivis ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"6", nombre:"neivis ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"7", nombre:"neivis ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" },
-  { id:"8", nombre:"neivis ", apellidos:"franco", tutor:"patricia Reyes", curso:"noveno" }
-]
 
-  constructor() { }
+  constructor(private documentoCurricularListService:DocumentoCurricularListService) { }
 
   ngOnInit(): void {
+    this.documentoCurricularListService.getDocumento()
+      .subscribe(documentos => this.documentoList= documentos.docs)
   }
+
+  ///cracion del pdf-------------- 
+  createPdf(){
+    const DATA = document.getElementById('myData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
+  
+  } 
 
 }
--- src/app/modules/estudiante/pages/student-list/student-list.component.ts
+++ src/app/modules/estudiante/pages/student-list/student-list.component.ts
@@ -211,42 +211,7 @@ export class StudentListComponent implements OnInit {
   }
 
 
-  ///cracion del pdf-------------- 
-   createPdf(){
-    const DATA = document.getElementById('myData');
-    const doc = new jsPDF('p', 'pt', 'a4');
-    const options = {
-      background: 'white',
-      scale: 3
-    };
-    html2canvas(DATA, options).then((canvas) => {
-
-      const img = canvas.toDataURL('image/PNG');
-
-      // Add image Canvas to PDF
-      const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
-    }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
-    });
   
-
-
-    //const pdfDefinition: any = {
-      //contenet:[
-        //{
-          //text: ''
-        //}
-      //]
-    //}
-    //const pdf = pdfMake.createPdf(pdfDefinition);
-    //pdf.open();
-  } 
  
 
 }
